cec327a7dd1acd1e9d0450496b588905541773b1
Akintayo Sinmiloluwa <akintayosinmi1@gmail.com>
2026-03-13 23:33:07 +0100

    regular commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#fun code
+print("I'm writing my first python program")
+print(5+2)
+# This is a comment haha
+print("Hello World")
+name = "Sinmi"
+print(name)
+firstValue = 2
+secondValue = 5
+print(firstValue + secondValue)
+def add(a, b):
+    return a + b
+print (add(5,7))
