18625c625eadf9e64681041a352e413e10cd229e
Akintayo Sinmiloluwa <akintayosinmi1@gmail.com>
2026-07-23 17:29:13 +0100

    added a new class file

---
+++ class9.py
@@ -0,0 +1,10 @@
+numbers = [0,1,2,3,4,5]
+print(numbers[::1])
+print(numbers[::-1])
+print(6 in numbers)
+print(3 in numbers)
+
+fruits = ["apple", "banana", "guava"]
+for fruit in fruits:
+    print(fruit)
+
